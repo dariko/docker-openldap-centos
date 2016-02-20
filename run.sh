8d13337f580dd35e0906dfca61f60091bbcb172e
dario <dario@dariozanzico.com>
2016-02-20 16:50:24 +0100

    add INIT_CONFIG parameter, correct slapd invokation

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 
 LOGLEVEL=${LOGLEVEL:-32768}
 
-/usr/local/openldap/libexec/slapd -h 'ldap://*:389 ldaps://*:636' -f /usr/local/openldap/etc/openldap/slapd.conf -u ldap -g ldap -d $LOGLEVEL
+
+if [ ! -z "$INIT_CONFIG" ];then
+  /init_config.sh
+fi
+
+
+/usr/local/openldap/libexec/slapd -h 'ldap://*:389 ldaps://*:636' -F /usr/local/openldap/etc/openldap/slapd.d -u ldap -g ldap -d $LOGLEVEL
