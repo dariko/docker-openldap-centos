c3180ff3d903434cb8b2484558066f1eb17755cb
dzanzico <dario.zanzico-c@postecom.it>
2016-03-07 15:10:27 +0100

    corrections to init_config_ldif.sh

--- init_config_ldif.sh
+++ init_config_ldif.sh
@@ -4,9 +4,21 @@ OLB=${OPENLDAP_BASE:-/usr/local/openldap}
 CONFIG_PATH=${CONFIG_PATH:-$OLB/etc/openldap/slapd.d/}
 SLAPADD="$OLB/sbin/slapadd"
 
-
+rm -rf /usr/local/openldap/etc/openldap/etc/slapd.d/c*
 
 tee > /tmp/config.ldif
+
+for db_path in $(cat /tmp/config.ldif |sed -n 's/^olcdbdirectory: \(.*\)$/\1/pI')
+do
+  mkdir -p "$db_path"
+  chown -R ldap:ldap "$db_path"
+done
+
+pid_file=$(sed -n 's/^olcpidfile: \(.*\)$/\1/pI' /tmp/config.ldif)
+pid_dir="$(dirname $pid_file)"
+rm -rf "$pid_file"
+mkdir -p "$pid_dir"
+chown -R ldap:ldap "$pid_dir"
 
 rm -rf "$CONFIG_PATH"/*
 
