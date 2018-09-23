f0832d53823cccdd28246e952299708ef144caf7
dariko <git@dariozanzico.com>
2018-09-23 13:48:28 +0200

    upgrade centos and openldap, cleanup

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 
 LOGLEVEL=${LOGLEVEL:-32768}
 
-
 if [ ! -z "$INIT_CONFIG" ];then
   /init_config.sh
 fi
@@ -11,6 +10,5 @@ SLAPD_ENDPOINTS="${SLAPD_ENDPOINTS:-ldap://*:389 ldaps://*:636}"
 
 chown -R ldap:ldap /usr/local/openldap/etc/openldap/slapd.d
 chown -R ldap:ldap /usr/local/openldap/var/openldap*
-
 
 /usr/local/openldap/libexec/slapd -h "$SLAPD_ENDPOINTS" -F /usr/local/openldap/etc/openldap/slapd.d -u ldap -g ldap -d $LOGLEVEL
